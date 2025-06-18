7c47d8c52b89b1d470dd1cf363417d3cb25509ee
Abhisheknayak-programer <sahiservice300@gmail.com>
2025-06-18 09:33:17 +0530

    Code updated

---
+++ Day 9/script.js
@@ -0,0 +1,67 @@
+console.log("Abhishek Nayak");
+console.log(190 + 10);
+console.error("Database connection broken");
+console.log(7 * 5 - 45 + 46);
+
+let a = 7 * 5 - 45 + 46;
+console.log(a);
+a = a + 5;
+console.log(a);
+
+const b = 7 * 5;
+console.log(b);
+// b = b + 5;
+// console.log(b);
+
+var c = 7 * 5 - 45 + 46;
+console.log(c);
+c = c + 5;
+console.log(c);
+
+// str, num, bool, undefined, null, object
+
+let empId = 7;
+console.log(empId);
+const isValid = true;
+console.log(isValid);
+let test;
+console.log(test);
+let val = null;
+console.log(val);
+let arr = [90, 10, 200];
+console.log(arr);
+
+console.log(9.477585859);
+console.log(Math.trunc(9.477585859));
+console.log(Math.floor(76.89));
+console.log(Math.ceil(76.89));
+console.log(Math.PI);
+console.log(Math.sqrt(25));
+console.log(Math.pow(2, 4));
+console.log((9.477585859).toFixed(2));
+console.log(Math.abs(19 - 2348576));
+
+let name = "Abhishek Nayak";
+console.log(name);
+name = 90;
+console.log(name);
+name = false;
+console.log(name);
+
+console.log(document);
+
+const childBox = document.getElementsByClassName("child_box");
+console.log(childBox);
+
+const testEl = document.getElementById("test");
+console.log(testEl);
+
+const childEl = document.querySelector(".child_box");
+const testBox = document.querySelector("#test");
+
+const allChilds = document.querySelectorAll(".child_box");
+console.log(allChilds);
+
+// Homeworks
+// 1. Create Some similiar classes and select it using query selector
+// 2. Create a product card like in myntra or flipkart use the Math for showing price in 2 decimals
